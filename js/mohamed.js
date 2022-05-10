cd1b990067f0f0d3bd0cc6d32175c2e564c74445
hnyteam <104102462+hnyteam@users.noreply.github.com>
2022-05-10 21:14:15 +0200

    Add files via upload

--- js/mohamed.js
+++ js/mohamed.js
@@ -14,3 +14,23 @@ if(bar){
      )
  }       
 
+ let img = document.querySelector('img');
+ let  btn1 = document.querySelector('#btn1');
+ let btn2 = document.querySelector('#btn2');
+ let btn3 = document.querySelector('#btn3');
+
+ btn1.addEventListener('click' , () => {
+
+    img.src = './media/css/secreat.jpg';
+ })
+
+
+ btn2.addEventListener('click' , () => {
+
+    img.src='./media/css/عندما-يعزف-الشيطان-18757(1).jpg';
+ })
+
+ btn3.addEventListener('click' ,() => {
+    
+    img.src='./media/css/yousef.jpeg';
+ })
